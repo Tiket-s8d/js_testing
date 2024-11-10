64407d090285342570cdb21773f10d38d3b2eccf
tik <timofei.nechaev@gmail.com>
2024-11-10 09:12:45 +0700

    Dot on grap don't duplicate

--- server/server.js
+++ server/server.js
@@ -5,16 +5,13 @@ const path = require('path');
 const app = express();
 const PORT = 3005;
 
-// Папка, где хранятся markdown файлы
 const markdownDir = path.join(__dirname, '../md');
 const dateRegex = /data created: (\d{4}-\d{2}-\d{2})/;
 
-// Кеширование данных
 let cachedData = null;
 let lastFetchTime = 0;
 const CACHE_EXPIRATION_TIME = 10 * 60 * 1000; // 10 минут
 
-// Получение данных о файлах
 function parseMarkdownFiles() {
     const currentYear = new Date().getFullYear();
     const currentTime = Date.now();
@@ -23,7 +20,7 @@ function parseMarkdownFiles() {
         return cachedData; // Возвращаем кешированные данные
     }
 
-    const dates = [];
+    const dates = new Set();
     const filesForDates = {};
 
     fs.readdirSync(markdownDir).forEach(file => {
@@ -35,7 +32,7 @@ function parseMarkdownFiles() {
                 const date = new Date(match[1]);
                 if (date.getFullYear() === currentYear) {
                     const dateStr = date.toISOString().split('T')[0]; // YYYY-MM-DD
-                    dates.push(dateStr);
+                    dates.add(dateStr);
                     if (!filesForDates[dateStr]) {
                         filesForDates[dateStr] = [];
                     }
@@ -45,13 +42,13 @@ function parseMarkdownFiles() {
         }
     });
 
-    cachedData = { dates, filesForDates };
+    // Преобразуем Set в массив
+    cachedData = { dates: Array.from(dates), filesForDates };
     lastFetchTime = currentTime;
 
     return cachedData;
 }
 
-// API для получения данных
 app.get('/data', (req, res) => {
     const data = parseMarkdownFiles();
     const result = {
@@ -61,7 +58,8 @@ app.get('/data', (req, res) => {
     res.json(result);
 });
 
-// Статичные файлы (HTML, CSS, JS)
+
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
